c64318bb19b687faf600134030efdd1d6c581fd8
MrMeow3141 <rlimeowmeowcat@gmail.com>
2024-08-18 12:22:37 -0400

    $ git commit -m "housing Order and Template"

---
+++ blegat.github.io-main/housingOrder.jl
@@ -0,0 +1,44 @@
+using JuMP
+using HiGHS
+# Let there be N communities, numbered from 1 to N, on a plane described by Cartesian coordinates. Let these be:
+S = [x_1 x_2 x_3 ... x_N;
+     y_1 y_2 y_3 ... y_N]
+# where (x_i, y_i) describes the location of the ith facility.
+# Let there be K separate locations for amenities, numbered 1 to K, of which there are P types, numbered 1 to P. Let the set of all amenities be:
+A = [t_1 t_2 t_3 ... t_P]
+# where each t_i is the 2d array of the coordinates of all the amenities of the ith type:
+t_i = [x_1 x_2 x_3 ... ;
+       y_1 y_2 y_3 ... ]
+P = length(A)
+# where t_i denotes the type of the ith amenity, and (x_i, y_i) the location.
+# We wish to select M of the N facility locations to become housing locations, in such a way that each housing location has optimized access to at least one amenity of each type. 
+selected = M
+# The weights for each amenity type are:
+D = [w_1 w_2 ... w_p]
+
+
+model = Model(HiGHS.Optimizer)
+@variable(model, x[com in 1:length(S[1,:]), place in 1:length(S[1,:])],Bin) # array describing the ranking of each community.
+
+@constraint(model, [com in 1:length(S[1,:])], sum(x[com,:])==1) # ensuring each community has 1 ranking
+@constraint(model, [place in 1:length(S[1,:])], sum(x[:,place]==1)) # ensuring each ranking has one community.
+
+@expression(model, c[house = 1:length(x), type = 1:P], minimum(abs(S[1,house]-A[type][1,ind ]) + abs(S[2,house]-A[type][2,ind]) for ind in 1:length(A[type][1]))) # this monster determines the shortest taxicab distance from a given neighbourhood's center to the nearest amenity of a given type. 
+
+for place in 1:(length(S[1,:])-1)
+    for com1 in 1:length(S[1,:])
+        if x[com1,place]==1
+            for com2 in 1:length(S[1,:])
+                if x[com2,place+1]==1
+                    @constraint(model, sum(c[com1,t]*D[t] for t in 1:P)>=sum(c[com2,t]*D[t] for t in 1:P)) # searches for two communities whose rankings differ by 1 and verifies that the community of higher ranking has a better evaluation score than that of the lower ranking. 
+                end
+            end
+        end
+    end
+end
+
+optimize!(model)
+
+solution_summary(model)
+
+num_variables(model)
